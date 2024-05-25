2097b57d09479100918dd5eecf1d0fb616836e7a
MpBlock <136390089+MpBlock@users.noreply.github.com>
2024-05-25 19:26:59 -0300

    Add files via upload

---
+++ sketch_pontH_lento.ino
@@ -0,0 +1,192 @@
+#include <Servo.h>          //standard library for the servo
+#include <NewPing.h>        //for the Ultrasonic sensor function library.
+
+//L298N motor control pins
+const int LeftMotorForward = 4;
+const int LeftMotorBackward = 5;
+const int RightMotorForward = 2;
+const int RightMotorBackward = 7;
+const int enA = 3;
+const int enB = 6;
+
+//sensor pins
+#define trig_pin 10 //
+#define echo_pin 11 //
+
+#define maximum_distance 200
+boolean goesForward = false;
+int distance = 100;
+
+NewPing sonar(trig_pin, echo_pin, maximum_distance); //sensor function
+Servo servo_motor; 
+
+void setup(){
+
+  pinMode(RightMotorForward, OUTPUT);
+  pinMode(LeftMotorForward, OUTPUT);
+  pinMode(LeftMotorBackward, OUTPUT);
+  pinMode(RightMotorBackward, OUTPUT);
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT); 
+  
+  servo_motor.attach(9); //our servo pin
+
+  servo_motor.write(115);
+  delay(1000);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+}
+
+void loop(){
+  analogWrite(enA,100);
+  analogWrite(enB,100);
+  int distanceRight = 0;
+  int distanceLeft = 0;
+  delay(50);
+  
+  if (distance <= 15){
+    moveStop();
+    delay(200);
+    moveBackward();
+    delay(200);
+    moveStop();
+    delay(200);
+    distanceRight = lookRight();
+    delay(200);
+    distanceLeft = lookLeft();
+    delay(200);
+
+    if (distance >= distanceLeft){
+      turnRight();
+      moveStop();
+    }
+    else{
+      turnLeft();
+      moveStop();
+    }
+  }
+  else{
+    moveForward(); 
+    
+  }
+    distance = readPing();
+}
+
+int lookRight(){  
+  servo_motor.write(50);
+  delay(200);
+  int distance = readPing();
+  delay(100);
+  servo_motor.write(115);
+  return distance;
+}
+
+int lookLeft(){
+  servo_motor.write(170);
+  delay(200);
+  int distance = readPing();
+  delay(100);
+  servo_motor.write(115);
+  return distance;
+  delay(100);
+}
+
+int readPing(){
+  delay(70);
+  int cm = sonar.ping_cm();
+  if (cm==0){
+    cm=250;
+  }
+  return cm;
+}
+
+void moveStop(){
+  
+  digitalWrite(RightMotorForward, LOW);
+  digitalWrite(LeftMotorForward, LOW);
+  digitalWrite(RightMotorBackward, LOW);
+  digitalWrite(LeftMotorBackward, LOW);
+  digitalWrite(enA,HIGH);
+  digitalWrite(enB,HIGH);
+}
+
+void moveForward(){
+
+  if(!goesForward){
+
+    goesForward=true;
+    
+    digitalWrite(LeftMotorForward, HIGH);
+    digitalWrite(RightMotorForward, HIGH);
+  
+    digitalWrite(LeftMotorBackward, LOW);
+    digitalWrite(RightMotorBackward, LOW); 
+    digitalWrite(enA,HIGH);
+    digitalWrite(enB,HIGH);
+  }
+}
+
+void moveBackward(){
+
+  goesForward=false;
+
+  digitalWrite(LeftMotorBackward, HIGH);
+  digitalWrite(RightMotorBackward, HIGH);
+  
+  digitalWrite(LeftMotorForward, LOW);
+  digitalWrite(RightMotorForward, LOW);
+  digitalWrite(enA,HIGH);
+  digitalWrite(enB,HIGH);
+  
+}
+
+void turnRight(){
+
+  digitalWrite(LeftMotorForward, HIGH);
+  digitalWrite(RightMotorBackward, HIGH);
+  
+  digitalWrite(LeftMotorBackward, LOW);
+  digitalWrite(RightMotorForward, LOW);
+  digitalWrite(enA,HIGH);
+  digitalWrite(enB,HIGH);
+  
+  delay(200);
+  
+  digitalWrite(LeftMotorForward, HIGH);
+  digitalWrite(RightMotorForward, HIGH);
+  
+  digitalWrite(LeftMotorBackward, LOW);
+  digitalWrite(RightMotorBackward, LOW);
+  digitalWrite(enA,HIGH);
+  digitalWrite(enB,HIGH);
+ 
+  
+  
+}
+
+void turnLeft(){
+
+  digitalWrite(LeftMotorBackward, HIGH);
+  digitalWrite(RightMotorForward, HIGH);
+  
+  digitalWrite(LeftMotorForward, LOW);
+  digitalWrite(RightMotorBackward, LOW);
+  digitalWrite(enA,HIGH);
+  digitalWrite(enB,HIGH);
+
+  delay(200);
+  
+  digitalWrite(LeftMotorForward, HIGH);
+  digitalWrite(RightMotorForward, HIGH);
+  
+  digitalWrite(LeftMotorBackward, LOW);
+  digitalWrite(RightMotorBackward, LOW);
+  digitalWrite(enA,HIGH);
+  digitalWrite(enB,HIGH);
+}
